b184deb4954b282e8fb00951c8f0178d91cc05ca
Tamara <myrka1106@mail.ru>
2023-02-06 19:30:02 +0300

    input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,22 @@
 
-fun main(args: Array<String>) {
-    println("Для начала работы введите команду /start")
+    fun main(){
+        println("Для начала работы введите команду /start")
+        while (true) {
+            val readLine = readLine()
+            when (readLine) {
+                "/start" -> {
+                    println("Здравствуйте")
+                    println("Введите команду /help для получения списка возможных команд")
+                }
+                "/help" -> println("/start - команда для приветстви, /end - команда для прекращения программы")
 
-    while (true) {
-        val readLine = readLine()
-        when (readLine) {
-            "/start" -> {
-                println("Здравствуйте")
-                println("Введите команду /help для получения списка возможных команд")
-            }
+                "/end" -> break
 
-            "/help" -> println("/start - команда для приветстви, /end - команда для прекращения программы")
-
-            "/end" -> break
-
-            else -> {
-                println("Программа не может отработать запрос, введите команду /help для получения списка возможных команд")
+                else -> {
+                    println("Программа не может отработать запрос, введите команду /help для получения списка возможных команд")
+                }
             }
         }
     }
 
-}
+
